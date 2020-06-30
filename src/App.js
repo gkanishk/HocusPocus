d55826c6d904b336746d5f5babe9c56c0fe98cb7
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-06-30 12:54:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import Shape from './Component/Shape'
 import './App.css';
 
 function App() {
+  var x;
+  var y;
+  useEffect(() => {
+    window.addEventListener('mousemove',(e)=>{
+      if("dataname" in e.target.attributes)
+      {var xv=parseInt(`${e.x}`)-parseInt(`${e.target.offsetLeft}`);
+      var yv=(parseInt(`${e.target.offsetTop}`)+parseInt(`${e.target.offsetHeight}`))-parseInt(`${e.y}`);
+      document.getElementById('sdata').innerHTML=`X: ${xv} Y: ${yv}`
+      console.log(xv+" "+x);
+      if(xv==x&&yv==y)
+      {
+        alert("Congratulations")
+      }
+    }
+    })
+  }, []);
+
+  const generateCord=()=>
+  {
+    x=(Math.floor(Math.random()*300));
+    y=(Math.floor(Math.random()*300));
+    document.getElementById("showCd").innerHTML=`X: ${x} Y: ${y}`;
+    console.log(x+" "+y);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hocus Pocus</h2>
+      <div className="getData">
+        <div className="showCord" id="showCd">Start</div>
+        <button id="btn" onClick={()=>generateCord()}>Generate</button>
+      </div>
+      <Shape/>
+      <h4>I am weak in maths please find help me find co-ordinates.</h4>
     </div>
   );
 }
---
+++ src/Component/Shape.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Shape() {
+    return (
+        <div className="ShapeF">
+        <div className="shapeC" dataname="square">
+        <div className="showData" id="sdata">
+        X: Y: 
+        </div>
+        </div>
+        </div>
+        
+    )
+}
+
+export default Shape
